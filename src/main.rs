af1ebb99c32f1598b96d53c71e10ed8ef397836e
Christopher Timm <chri_ti1991@yahoo.de>
2022-07-19 23:42:24 -0500

    Group existing files by their hash
    Also allow finding more than two files that are identical.
    
    Also allow supplying more than two directories to compare to oneanother
    
    Also ignore directories

--- src/main.rs
+++ src/main.rs
@@ -4,51 +4,60 @@ use clap::Parser;
 use glob::glob;
 use sha2::{Digest, Sha256};
 use sha2::digest::Output;
-use std::{fs,io};
+use std::{fs, io};
+use std::collections::{HashMap};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(long, value_parser, default_value = ".")]
-    source: String,
+    base: String,
 
-    #[clap(long, value_parser)]
-    target: String,
+    #[clap(short, long, value_parser)]
+    additional: Vec<String>,
 }
 
 fn main() {
-    let args = Args::parse();
-    let parsed_source = Path::new(&args.source);
-    let parsed_target = Path::new(&args.target);
+    let args: Args = Args::parse();
+    let parsed_source = Path::new(&args.base);
+    let parsed_targets = &args.additional.iter().map(|path| Path::new(path)).collect::<Vec<&Path>>();
     if !parsed_source.exists() {
         panic!("source directory doesn't exist")
     }
-    if !parsed_target.exists() {
-        panic!("target directory doesn't exist")
+    let non_existing_directories: Vec<&Path> = parsed_targets.iter().filter(|path| !path.exists()).map(|path| *path).collect();
+    if !non_existing_directories.is_empty() {
+        panic!("The supplied target directories {:?} don't exist", non_existing_directories)
     }
-    println!("Comparing directory {} with {}", parsed_source.display(), parsed_target.display());
+    println!("Comparing directory {} and {:?}", parsed_source.display(), parsed_targets);
 
-    let _all_source_files = get_all_files_in_directory(parsed_source);
-    let _all_target_files = get_all_files_in_directory(parsed_target);
+    let mut all_files = HashMap::new();
+    add_all_files_in_directory(parsed_source, &mut all_files);
+    parsed_targets.iter().for_each(|parsed_target| add_all_files_in_directory(parsed_target, &mut all_files));
+    let mapped: Vec<(&Output<Sha256>, &Vec<PathBuf>)> = all_files.iter()
+        .filter(|(key, value)| !value.is_empty())
+        .collect();
+
+    println!("hash: {:?}", mapped);
 }
 
-fn get_all_files_in_directory(parsed_source: &Path) -> Vec<(PathBuf, Output<Sha256>)> {
+fn add_all_files_in_directory(parsed_source: &Path, all_files: &mut HashMap<Output<Sha256>, Vec<PathBuf>>) {
     let mut string = parsed_source.display().to_string();
     string.push_str("/**/*");
-    let mut path_bufs = vec![];
     for entry in glob(string.as_str()).expect("Failed to read glob pattern") {
         match entry {
             Ok(path) => {
+                if path.is_dir() {
+                    return;
+                }
                 let mut file = fs::File::open(&path).unwrap();
                 let mut hasher = Sha256::new();
                 io::copy(&mut file, &mut hasher).unwrap();
                 let hash = hasher.finalize();
                 let input = hash.as_ref();
-                println!("{:?} - {:?}", path.display(), Base64::encode_string(input));
-                path_bufs.push((path, hash));
+                println!(" Discovered file: {:?} - {:?}", path.display(), Base64::encode_string(input));
+                all_files.entry(hash).or_insert(Vec::new()).push(path);
             },
             Err(e) => println!("{:?}", e),
         }
     }
-    path_bufs
 }
